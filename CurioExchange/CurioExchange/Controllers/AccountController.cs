6ed16f1de1200e7fde8248c461757077bc4c454f
Nina Arens <ninaarens@hotmail.com>
2014-09-02 01:38:50 +0200

    Login changes

--- CurioExchange/CurioExchange/Controllers/AccountController.cs
+++ CurioExchange/CurioExchange/Controllers/AccountController.cs
@@ -159,7 +159,7 @@ namespace CurioExchange.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Name, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
