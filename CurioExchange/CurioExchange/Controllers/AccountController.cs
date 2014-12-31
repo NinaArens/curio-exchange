2b5a97ddeadb866f6605e996503c431c51d5f9e3
Nina Arens <ninaarens@hotmail.com>
2014-12-31 01:49:54 +0100

    Password recovery fix

--- CurioExchange/CurioExchange/Controllers/AccountController.cs
+++ CurioExchange/CurioExchange/Controllers/AccountController.cs
@@ -228,11 +228,11 @@ namespace CurioExchange.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = await UserManager.FindByNameAsync(model.Email);
-                if (user == null || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+                var user = await UserManager.FindByEmailAsync(model.Email);
+                if (user == null)
                 {
-                    // Don't reveal that the user does not exist or is not confirmed
-                    return View("ForgotPasswordConfirmation");
+                    ViewBag.errorMessage = "No user with this email address exists.";
+                    return View("Error");
                 }
 
                 // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
